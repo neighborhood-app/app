e49861b77f31e5d167a91285924055a7b0cbc78f
Radu <raduadrian.constantin94@gmail.com>
2023-03-13 20:37:57 +0200

    Updated schema.sql with changes made by Bob

--- src/model/schema.sql
+++ src/model/schema.sql
@@ -28,10 +28,25 @@ SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
+CREATE TABLE public.genders (
+    id integer NOT NULL,
+    name character varying(10)
+);
+
+CREATE SEQUENCE public.genders_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER SEQUENCE public.genders_id_seq OWNED BY public.genders.id;
+
 CREATE TABLE public.neighborhoods (
     id integer NOT NULL,
     admin_id numeric NOT NULL,
-    name character varying(50),
+    name character varying(50) NOT NULL,
     description character varying(50),
     location character varying(50)
 );
@@ -44,8 +59,6 @@ CREATE SEQUENCE public.neighborhoods_id_seq
     NO MAXVALUE
     CACHE 1;
 
-ALTER SEQUENCE public.neighborhoods_id_seq OWNED BY public.neighborhoods.id;
-
 CREATE TABLE public.neighborhoods_users (
     neighborhood_id integer NOT NULL,
     user_id integer NOT NULL,
@@ -56,7 +69,8 @@ CREATE TABLE public.requests (
     id integer NOT NULL,
     neighborhood_id integer NOT NULL,
     user_id integer NOT NULL,
-    content character varying(1000),
+    title character varying(50) NOT NULL,
+    content character varying(1000) NOT NULL,
     status integer,
     time_created timestamp with time zone
 );
@@ -97,7 +111,7 @@ CREATE TABLE public.users (
     first_name character varying(25),
     last_name character varying(25),
     dob date,
-    gender numeric,
+    gender_id integer,
     bio character varying(500)
 );
 
@@ -111,6 +125,8 @@ CREATE SEQUENCE public.users_id_seq
 
 ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
+ALTER TABLE ONLY public.genders ALTER COLUMN id SET DEFAULT nextval('public.genders_id_seq'::regclass);
+
 ALTER TABLE ONLY public.neighborhoods ALTER COLUMN id SET DEFAULT nextval('public.neighborhoods_id_seq'::regclass);
 
 ALTER TABLE ONLY public.requests ALTER COLUMN id SET DEFAULT nextval('public.requests_id_seq'::regclass);
@@ -118,6 +134,39 @@ ALTER TABLE ONLY public.requests ALTER COLUMN id SET DEFAULT nextval('public.req
 ALTER TABLE ONLY public.responses ALTER COLUMN id SET DEFAULT nextval('public.responses_id_seq'::regclass);
 
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+COPY public.genders (id, name) FROM stdin;
+1	Male
+2	Female
+\.
+
+COPY public.neighborhoods (id, admin_id, name, description, location) FROM stdin;
+\.
+
+COPY public.neighborhoods_users (neighborhood_id, user_id, active) FROM stdin;
+\.
+
+COPY public.requests (id, neighborhood_id, user_id, title, content, status, time_created) FROM stdin;
+\.
+
+COPY public.responses (id, request_id, user_id, content, status, time_created) FROM stdin;
+\.
+
+COPY public.users (id, user_name, password, first_name, last_name, dob, gender_id, bio) FROM stdin;
+\.
+
+SELECT pg_catalog.setval('public.genders_id_seq', 2, true);
+
+SELECT pg_catalog.setval('public.neighborhoods_id_seq', 1, false);
+
+SELECT pg_catalog.setval('public.requests_id_seq', 1, false);
+
+SELECT pg_catalog.setval('public.responses_id_seq', 1, false);
+
+SELECT pg_catalog.setval('public.users_id_seq', 1, false);
+
+ALTER TABLE ONLY public.genders
+    ADD CONSTRAINT genders_pkey PRIMARY KEY (id);
 
 ALTER TABLE ONLY public.neighborhoods
     ADD CONSTRAINT neighborhoods_pkey PRIMARY KEY (id);
@@ -152,3 +201,5 @@ ALTER TABLE ONLY public.responses
 ALTER TABLE ONLY public.responses
     ADD CONSTRAINT responses_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
 
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_gender_fkey FOREIGN KEY (gender_id) REFERENCES public.genders(id);
