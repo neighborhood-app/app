9ae29f484081c4e59c51980e3774ba0deb28f68d
nina-dk <antwnina.k@gmail.com>
2023-03-16 17:16:03 +0200

    Connect docker postgres to pg module; resolve #12

--- src/model/schema.sql
+++ src/model/schema.sql
@@ -1,39 +1,9 @@
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-CREATE DATABASE neighborhood WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'C';
-
-\connect neighborhood
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
-CREATE TABLE public.genders (
+CREATE TABLE genders (
     id integer NOT NULL,
     name character varying(10)
 );
 
-CREATE SEQUENCE public.genders_id_seq
+CREATE SEQUENCE genders_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -41,9 +11,7 @@ CREATE SEQUENCE public.genders_id_seq
     NO MAXVALUE
     CACHE 1;
 
-ALTER SEQUENCE public.genders_id_seq OWNED BY public.genders.id;
-
-CREATE TABLE public.neighborhoods (
+CREATE TABLE neighborhoods (
     id integer NOT NULL,
     admin_id numeric NOT NULL,
     name character varying(50) NOT NULL,
@@ -51,7 +19,7 @@ CREATE TABLE public.neighborhoods (
     location character varying(50)
 );
 
-CREATE SEQUENCE public.neighborhoods_id_seq
+CREATE SEQUENCE neighborhoods_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -59,13 +27,13 @@ CREATE SEQUENCE public.neighborhoods_id_seq
     NO MAXVALUE
     CACHE 1;
 
-CREATE TABLE public.neighborhoods_users (
+CREATE TABLE neighborhoods_users (
     neighborhood_id integer NOT NULL,
     user_id integer NOT NULL,
     active boolean
 );
 
-CREATE TABLE public.requests (
+CREATE TABLE requests (
     id integer NOT NULL,
     neighborhood_id integer NOT NULL,
     user_id integer NOT NULL,
@@ -75,7 +43,7 @@ CREATE TABLE public.requests (
     time_created timestamp with time zone
 );
 
-CREATE SEQUENCE public.requests_id_seq
+CREATE SEQUENCE requests_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -83,9 +51,7 @@ CREATE SEQUENCE public.requests_id_seq
     NO MAXVALUE
     CACHE 1;
 
-ALTER SEQUENCE public.requests_id_seq OWNED BY public.requests.id;
-
-CREATE TABLE public.responses (
+CREATE TABLE responses (
     id integer NOT NULL,
     request_id integer NOT NULL,
     user_id integer NOT NULL,
@@ -94,7 +60,7 @@ CREATE TABLE public.responses (
     time_created timestamp with time zone
 );
 
-CREATE SEQUENCE public.responses_id_seq
+CREATE SEQUENCE responses_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -102,9 +68,7 @@ CREATE SEQUENCE public.responses_id_seq
     NO MAXVALUE
     CACHE 1;
 
-ALTER SEQUENCE public.responses_id_seq OWNED BY public.responses.id;
-
-CREATE TABLE public.users (
+CREATE TABLE users (
     id integer NOT NULL,
     user_name character varying(25) NOT NULL,
     password character varying(25) NOT NULL,
@@ -115,7 +79,7 @@ CREATE TABLE public.users (
     bio character varying(500)
 );
 
-CREATE SEQUENCE public.users_id_seq
+CREATE SEQUENCE users_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -123,83 +87,49 @@ CREATE SEQUENCE public.users_id_seq
     NO MAXVALUE
     CACHE 1;
 
-ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+ALTER SEQUENCE users_id_seq OWNED BY users.id;
 
-ALTER TABLE ONLY public.genders ALTER COLUMN id SET DEFAULT nextval('public.genders_id_seq'::regclass);
+ALTER TABLE ONLY genders ALTER COLUMN id SET DEFAULT nextval('genders_id_seq'::regclass);
+ALTER TABLE ONLY neighborhoods ALTER COLUMN id SET DEFAULT nextval('neighborhoods_id_seq'::regclass);
+ALTER TABLE ONLY requests ALTER COLUMN id SET DEFAULT nextval('requests_id_seq'::regclass);
+ALTER TABLE ONLY responses ALTER COLUMN id SET DEFAULT nextval('responses_id_seq'::regclass);
+ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
 
-ALTER TABLE ONLY public.neighborhoods ALTER COLUMN id SET DEFAULT nextval('public.neighborhoods_id_seq'::regclass);
-
-ALTER TABLE ONLY public.requests ALTER COLUMN id SET DEFAULT nextval('public.requests_id_seq'::regclass);
-
-ALTER TABLE ONLY public.responses ALTER COLUMN id SET DEFAULT nextval('public.responses_id_seq'::regclass);
-
-ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
-
-COPY public.genders (id, name) FROM stdin;
-1	Male
-2	Female
-\.
-
-COPY public.neighborhoods (id, admin_id, name, description, location) FROM stdin;
-\.
-
-COPY public.neighborhoods_users (neighborhood_id, user_id, active) FROM stdin;
-\.
-
-COPY public.requests (id, neighborhood_id, user_id, title, content, status, time_created) FROM stdin;
-\.
-
-COPY public.responses (id, request_id, user_id, content, status, time_created) FROM stdin;
-\.
-
-COPY public.users (id, user_name, password, first_name, last_name, dob, gender_id, bio) FROM stdin;
-\.
-
-SELECT pg_catalog.setval('public.genders_id_seq', 2, true);
-
-SELECT pg_catalog.setval('public.neighborhoods_id_seq', 1, false);
-
-SELECT pg_catalog.setval('public.requests_id_seq', 1, false);
-
-SELECT pg_catalog.setval('public.responses_id_seq', 1, false);
-
-SELECT pg_catalog.setval('public.users_id_seq', 1, false);
-
-ALTER TABLE ONLY public.genders
+ALTER TABLE ONLY genders
     ADD CONSTRAINT genders_pkey PRIMARY KEY (id);
 
-ALTER TABLE ONLY public.neighborhoods
+ALTER TABLE ONLY neighborhoods
     ADD CONSTRAINT neighborhoods_pkey PRIMARY KEY (id);
 
-ALTER TABLE ONLY public.neighborhoods_users
+ALTER TABLE ONLY neighborhoods_users
     ADD CONSTRAINT neighborhoods_users_pkey PRIMARY KEY (neighborhood_id, user_id);
 
-ALTER TABLE ONLY public.requests
+ALTER TABLE ONLY requests
     ADD CONSTRAINT requests_pkey PRIMARY KEY (id);
 
-ALTER TABLE ONLY public.responses
+ALTER TABLE ONLY responses
     ADD CONSTRAINT responses_pkey PRIMARY KEY (id);
 
-ALTER TABLE ONLY public.users
+ALTER TABLE ONLY users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
 
-ALTER TABLE ONLY public.neighborhoods_users
-    ADD CONSTRAINT neighborhoods_users_neighborhood_id_fkey FOREIGN KEY (neighborhood_id) REFERENCES public.neighborhoods(id) ON DELETE CASCADE;
+ALTER TABLE ONLY neighborhoods_users
+    ADD CONSTRAINT neighborhoods_users_neighborhood_id_fkey FOREIGN KEY (neighborhood_id) REFERENCES neighborhoods(id) ON DELETE CASCADE;
 
-ALTER TABLE ONLY public.neighborhoods_users
-    ADD CONSTRAINT neighborhoods_users_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
+ALTER TABLE ONLY neighborhoods_users
+    ADD CONSTRAINT neighborhoods_users_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
 
-ALTER TABLE ONLY public.requests
-    ADD CONSTRAINT requests_neighborhood_id_fkey FOREIGN KEY (neighborhood_id) REFERENCES public.neighborhoods(id) ON DELETE CASCADE;
+ALTER TABLE ONLY requests
+    ADD CONSTRAINT requests_neighborhood_id_fkey FOREIGN KEY (neighborhood_id) REFERENCES neighborhoods(id) ON DELETE CASCADE;
 
-ALTER TABLE ONLY public.requests
-    ADD CONSTRAINT requests_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
+ALTER TABLE ONLY requests
+    ADD CONSTRAINT requests_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
 
-ALTER TABLE ONLY public.responses
-    ADD CONSTRAINT responses_request_id_fkey FOREIGN KEY (request_id) REFERENCES public.requests(id) ON DELETE CASCADE;
+ALTER TABLE ONLY responses
+    ADD CONSTRAINT responses_request_id_fkey FOREIGN KEY (request_id) REFERENCES requests(id) ON DELETE CASCADE;
 
-ALTER TABLE ONLY public.responses
-    ADD CONSTRAINT responses_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
+ALTER TABLE ONLY responses
+    ADD CONSTRAINT responses_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
 
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_gender_fkey FOREIGN KEY (gender_id) REFERENCES public.genders(id);
+ALTER TABLE ONLY users
+    ADD CONSTRAINT users_gender_fkey FOREIGN KEY (gender_id) REFERENCES genders(id);
