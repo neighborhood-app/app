84635f3eafacadfffb5d8eb37e48c0ab3ddb295d
Radu <raduadrian.constantin94@gmail.com>
2023-03-22 06:49:36 +0200

    Updated package-json scripts (not finished)

--- src/model/db_query.ts
+++ src/model/db_query.ts
@@ -12,6 +12,7 @@ export default async function dbQuery(
   statement: string,
   ...parameters: string[]
 ) {
+  console.log(config.POSTGRES_URL); //REMOVE AFTER TESTING
   const client = new Client(config.POSTGRES_URL);
 
   await client.connect();
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,4 +2,8 @@ import { config } from 'dotenv';
 
 config({ path: `${__dirname}/../../.env` });
 
+process.env.POSTGRES_URL = process.env.NODE_ENV === 'development'
+  ? process.env.DEV_POSTGRES_URL
+  : process.env.TEST_POSTGRES_URL;
+
 export default process.env;
