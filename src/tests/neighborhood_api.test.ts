96fe5a645a7f7b09f26dd95b04de81d5a889cdc0
Radu <raduadrian.constantin94@gmail.com>
2023-03-23 06:57:47 +0200

    Working test

--- src/tests/neighborhood_api.test.ts
+++ src/tests/neighborhood_api.test.ts
@@ -1,3 +1,13 @@
-describe('Testing GET method for neighborhood API.', () => {
+import app from '../../app';
 
+const supertest = require('supertest'); // eslint-disable-line
+// require was used because supertest does not support import
+
+const api = supertest(app);
+
+describe('Testing GET method for neighborhood API.', () => {
+  test('All neighborhoods are returned', async () => {
+    const response = await api.get('/neighborhoods');
+    expect(response.status).toEqual(200);
+  });
 });
