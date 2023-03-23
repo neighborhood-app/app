5968fe46545251e99a451736207a5a66da82c57e
Radu <raduadrian.constantin94@gmail.com>
2023-03-23 06:57:47 +0200

    Working test

--- app.ts
+++ app.ts
@@ -10,6 +10,7 @@ app.get('/', (_req, res) => {
 });
 
 app.get('/neighborhoods', async (_req, res) => {
+  console.log('Works');
   const neighborhoods = await new Neighborhood().getAllNeighborhoods();
   if (!neighborhoods) return res.sendStatus(404);
 
