210501b42e1bdae9990f1307095be2cc8cc2ae73
Radu <raduadrian.constantin94@gmail.com>
2023-03-26 14:29:36 +0300

    Added users and neighborhoods in seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,23 +3,96 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.createMany({
+  await prisma.user.deleteMany({});
+  await prisma.neighborhood.deleteMany({});
+  await prisma.neighborhoodUsers.deleteMany({});
+  await prisma.gender.deleteMany({});
+  await prisma.request.deleteMany({});
+  await prisma.request.deleteMany({});
+
+  await prisma.gender.createMany({
     data: [
       {
-        userName: 'bob',
-        password: 'example',
+        name: 'male',
       },
       {
-        userName: 'antonina',
-        password: 'example',
+        name: 'female',
+      },
+    ],
+  });
+
+  const bob = await prisma.user.create({
+    data: {
+      userName: 'bob',
+      password: 'example',
+    },
+  });
+
+  const antonina = await prisma.user.create({
+    data: {
+      userName: 'antonina',
+      password: 'example',
+    },
+  });
+
+  const shwetank = await prisma.user.create({
+    data: {
+      userName: 'shwetank',
+      password: 'example',
+    },
+  });
+
+  const radu = await prisma.user.create({
+    data: {
+      userName: 'radu',
+      password: 'example',
+    },
+  });
+
+  const mike = await prisma.user.create({
+    data: {
+      userName: 'mike',
+      password: 'example',
+    },
+  });
+
+  const maria = await prisma.user.create({
+    data: {
+      userName: 'maria',
+      password: 'example',
+    },
+  });
+
+  const bobNeighborhood = await prisma.neighborhood.create({
+    data: {
+      adminID: bob.id,
+      name: "Bob's Neighborhood",
+    },
+  });
+
+  const antoninaNeighborhood = await prisma.neighborhood.create({
+    data: {
+      adminID: antonina.id,
+      name: "Antonina's Neighborhood",
+    },
+  });
+
+  const shwetankNeighborhood = await prisma.neighborhood.create({
+    data: {
+      adminID: shwetank.id,
+      name: "Shwetank's Neighborhood",
+    },
+  });
+
+  await prisma.neighborhoodUsers.createMany({
+    data: [
+      {
+        neighborhoodID: bobNeighborhood.id,
+        userID: radu.id,
       },
       {
-        userName: 'shwetank',
-        password: 'example',
-      },
-      {
-        userName: 'radu',
-        password: 'example',
+        neighborhoodID: bobNeighborhood.id,
+        userID: maria.id,
       },
     ],
   });
