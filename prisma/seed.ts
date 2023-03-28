8cf776f69510b6ebd6c2f25dec6e07d0712de5c3
Radu <raduadrian.constantin94@gmail.com>
2023-03-28 19:28:46 +0300

    Added query log to seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({ log: ['query'] });
 
 async function main() {
   await prisma.user.deleteMany({});
