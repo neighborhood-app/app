013149e72e9ca0ffce10f7f1197893f0f83682e7
Radu <raduadrian.constantin94@gmail.com>
2023-03-29 10:34:10 +0300

    Switched to multiple env files for different environments

--- src/tests/neighborhood_api.test.ts
+++ src/tests/neighborhood_api.test.ts
@@ -6,16 +6,17 @@ import config from '../utils/config';
 const supertest = require('supertest'); // eslint-disable-line
 // 'require' was used because supertest does not support import
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: { url: config.POSTGRES_URL },
-  },
-});
+const prisma = new PrismaClient();
 
 const request = supertest(app);
 
 beforeAll(async () => {
-
+  console.log(process.env.DATABASE_URL);
+  await prisma.user.deleteMany({});
+  await prisma.neighborhood.deleteMany({});
+  await prisma.gender.deleteMany({});
+  await prisma.request.deleteMany({});
+  await prisma.response.deleteMany({});
 });
 
 describe('Testing GET method for neighborhood API.', () => {
