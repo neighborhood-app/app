9fcd0e977baf9628bd97c7d04557bb0fd2ca09a7
Radu <raduadrian.constantin94@gmail.com>
2023-03-29 10:34:10 +0300

    Switched to multiple env files for different environments

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,10 +2,10 @@ import { config } from 'dotenv';
 
 config();
 
-const POSTGRES_URL = process.env.NODE_ENV === 'development'
+const DATABASE_URL = process.env.NODE_ENV === 'development'
   ? process.env.DEV_POSTGRES_URL
   : process.env.TEST_POSTGRES_URL;
 
 const { PORT } = process.env;
 
-export default { POSTGRES_URL, PORT };
+export default { DATABASE_URL, PORT };
