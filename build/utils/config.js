4cf18297ae1cb1e6ff4b7cd4acfb8ce21d81360f
Radu <raduadrian.constantin94@gmail.com>
2023-04-04 17:10:46 +0300

    Removed build folder and added it to gitignore

--- build/utils/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-const { DATABASE_URL, PORT } = process.env;
-exports.default = { DATABASE_URL, PORT };
