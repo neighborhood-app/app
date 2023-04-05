29e43c2cab220b35395c1803c8fbeff1596948e2
nina-dk <antwnina.k@gmail.com>
2023-04-05 13:35:15 +0300

    Create catch-error module and default express error handler

--- build/src/utils/config.js
+++ build/src/utils/config.js
@@ -1,8 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)({ path: `${__dirname}/../../.env` });
-process.env.POSTGRES_URL = process.env.NODE_ENV === 'development'
-    ? process.env.DEV_POSTGRES_URL
-    : process.env.TEST_POSTGRES_URL;
-exports.default = process.env;
+(0, dotenv_1.config)();
+const { DATABASE_URL, PORT } = process.env;
+exports.default = { DATABASE_URL, PORT };
