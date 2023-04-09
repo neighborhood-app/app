f03f173ad9915101b7e4cf8e1a78d98bd2ee9f38
shwetank <shwetank.tewari.87@gmail.com>
2023-04-09 15:12:02 -0400

    able to create user, but no input validation

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
+import bcrypt from 'bcrypt';
 import catchError from '../utils/catchError';
 import prisma from '../model/prismaClient';
 
@@ -8,8 +9,19 @@ usersRouter.get('/', catchError(async (_req: Request, res: Response) => {
   res.status(200).send('users will be fetched');
 }));
 
-usersRouter.post('/', catchError(async (_req: Request, res: Response) => {
-  res.status(201).send('new user will be created');
+usersRouter.post('/', catchError(async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  const SALT_ROUNDS = 10;
+  const passwordHash = await bcrypt.hash(password, SALT_ROUNDS);
+
+  const userData = {
+    user_name: username,
+    password_hash: passwordHash,
+  };
+
+  const savedUser = await prisma.user.create({ data: userData });
+  res.status(201).json(savedUser);
 }));
 
 export default usersRouter;
