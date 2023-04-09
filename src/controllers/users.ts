797eb0c0fde0623eb41ff0ae040783f40a3a28ca
shwetank <shwetank.tewari.87@gmail.com>
2023-04-09 14:50:10 -0400

    created dummy POST /api/users route

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -8,4 +8,8 @@ usersRouter.get('/', catchError(async (_req: Request, res: Response) => {
   res.status(200).send('users will be fetched');
 }));
 
+usersRouter.post('/', catchError(async (_req: Request, res: Response) => {
+  res.status(201).send('new user will be created');
+}));
+
 export default usersRouter;
