367d0f067145f93ef2fef06c32a69baa5c678fac
shwetank <shwetank.tewari.87@gmail.com>
2023-04-11 14:26:26 -0400

    removed redundant type

--- src/types.ts
+++ src/types.ts
@@ -2,8 +2,6 @@ import { User } from '@prisma/client';
 
 export type UserWithoutPasswordHash = Omit<User, 'password_hash'>;
 
-export type NewUserData = Omit<User, 'id'>;
-
 export type UserWithoutId = Omit<User, 'id'>;
 
 export interface LoginData {
