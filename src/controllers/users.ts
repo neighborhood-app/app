a9dcbe56828ecf7e168663b10861a2ff9f27c11a
shwetank <shwetank.tewari.87@gmail.com>
2023-04-11 18:39:14 -0400

    created simple GET /users and GET /users/:id route without any authentication, filtering or join queries

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -7,7 +7,24 @@ import routeHelpers from '../utils/routeHelpers';
 const usersRouter = express.Router();
 
 usersRouter.get('/', catchError(async (_req: Request, res: Response) => {
-  res.status(200).send('users will be fetched');
+  const users: Array<User> = await prismaClient.user.findMany({});
+  const usersWithoutPasswordHash = users.map(user => routeHelpers.getUserWithoutPasswordHash(user));
+  res.status(200).json(usersWithoutPasswordHash);
+}));
+
+usersRouter.get('/:id', catchError(async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const user = await prismaClient.user.findUnique({
+    where: {
+      id,
+    },
+  });
+  if (user) {
+    const userWithoutPasswordHash = routeHelpers.getUserWithoutPasswordHash(user);
+    res.status(200).json(userWithoutPasswordHash);
+  } else {
+    res.status(404).json({ error: 'user not found' });
+  }
 }));
 
 usersRouter.post('/', catchError(async (req: Request, res: Response) => {
