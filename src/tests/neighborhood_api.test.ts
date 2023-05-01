ec53816c1dc11b3a50cba857243a6760e6da2d0c
shwetank <shwetank.tewari.87@gmail.com>
2023-05-01 19:01:34 -0400

    moved getting neighborhoods in test to a helper function

--- src/tests/neighborhood_api.test.ts
+++ src/tests/neighborhood_api.test.ts
@@ -3,13 +3,20 @@ import app from '../app';
 import prismaClient from '../../prismaClient';
 import seed from './seed';
 import testHelpers from './testHelpers';
+import { LoginData } from '../types';
+import routeHelpers from '../utils/routeHelpers';
 
 const supertest = require('supertest'); // eslint-disable-line
 // 'require' was used because supertest does not support import
 
 const api = supertest(app);
 
-const loginData = {
+const loginData: LoginData = {
+  username: 'bob',
+  password: 'secret',
+};
+
+const LOGIN_DATA: LoginData = {
   username: 'bob',
   password: 'secret',
 };
@@ -24,50 +31,64 @@ describe('When neighborhoods already exist in the db', () => {
   });
 
   test('GET /neighborhoods returns all neighborhoods', async () => {
-    const neighborhoods = await prismaClient.neighborhood.findMany({});
+    const neighborhoods = await testHelpers.neighborhoodsInDb();
     const numberOfNeighborhoods = neighborhoods.length;
     const response = await api.get('/api/neighborhoods');
     expect(response.status).toEqual(200);
-    // expect(response.body.length).toEqual(3);  // 3 was a magic number.
     expect(response.body.length).toEqual(numberOfNeighborhoods);
   });
 
   test('DELETE /neighborhoods/:id removes a neighborhood', async () => {
-    const initialNeighborhoods = await api.get('/api/neighborhoods');
+    const initialNeighborhoods = await testHelpers.neighborhoodsInDb();
+    const numInitialNeighborhoods = initialNeighborhoods.length;
+
     const loginResponse = await api
       .post('/api/login')
-      .send(loginData);
+      .send(LOGIN_DATA);
+
     const { token } = loginResponse.body;
 
-    const response = await api.delete('/api/neighborhoods/1').set('Authorization', `Bearer ${token}`);
-    const currentNeighborhoods = await api.get('/api/neighborhoods');
+    // 1 is valid neighborhood id for LOGIN_DATA
+    const deleteResponse = await api.delete('/api/neighborhoods/1')
+      .set('Authorization', `Bearer ${token}`);
 
-    expect(response.status).toEqual(200);
-    expect(currentNeighborhoods.body.length).toEqual(initialNeighborhoods.body.length - 1);
+    const currentNeighborhoods = await testHelpers.neighborhoodsInDb();
+    const numCurrentNeighborhoods = currentNeighborhoods.length;
+
+    expect(deleteResponse.status).toEqual(200);
+    expect(numCurrentNeighborhoods).toEqual(numInitialNeighborhoods - 1);
   });
 
   test('User cannot delete neighborhood if he/she is not admin', async () => {
-    const initialNeighborhoods = await api.get('/api/neighborhoods');
+    const initialNeighborhoods = await testHelpers.neighborhoodsInDb();
+    const numInitialNeighborhoods = initialNeighborhoods.length;
+
     const loginResponse = await api
       .post('/api/login')
-      .send(loginData);
+      .send(LOGIN_DATA);
     const { token } = loginResponse.body;
 
-    const response = await api.delete('/api/neighborhoods/2').set('Authorization', `Bearer ${token}`);
-    const currentNeighborhoods = await api.get('/api/neighborhoods');
+    // 2 is invalid neighborhood id for LOGIN_DATA
+    const deleteResponse = await api.delete('/api/neighborhoods/2').set('Authorization', `Bearer ${token}`);
 
-    expect(response.status).toEqual(403);
-    expect(currentNeighborhoods.body.length).toEqual(initialNeighborhoods.body.length);
+    const currentNeighborhoods = await testHelpers.neighborhoodsInDb();
+    const numCurrentNeighborhoods = currentNeighborhoods.length;
+
+    expect(deleteResponse.status).toEqual(403);
+    expect(numCurrentNeighborhoods).toEqual(numInitialNeighborhoods);
   });
 
   test('User cannot delete neighborhood if he/she is not logged in', async () => {
-    const initialNeighborhoods = await api.get('/api/neighborhoods');
+    const initialNeighborhoods = await testHelpers.neighborhoodsInDb();
+    const numInitialNeighborhoods = initialNeighborhoods.length;
 
-    const response = await api.delete('/api/neighborhoods/2');
-    const currentNeighborhoods = await api.get('/api/neighborhoods');
+    const deleteResponse = await api.delete('/api/neighborhoods/1');
 
-    expect(response.status).toEqual(401);
-    expect(currentNeighborhoods.body.length).toEqual(initialNeighborhoods.body.length);
+    const currentNeighborhoods = await testHelpers.neighborhoodsInDb();
+    const numCurrentNeighborhoods = currentNeighborhoods.length;
+
+    expect(deleteResponse.status).toEqual(401);
+    expect(numInitialNeighborhoods).toEqual(numCurrentNeighborhoods);
   });
 });
 
