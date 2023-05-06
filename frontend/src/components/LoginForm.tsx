701e261c6fb9065a321a24e65206c40876fbc5a1
Radu <raduadrian.constantin94@gmail.com>
2023-05-06 08:59:27 +0300

    Initial login form styling

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,13 +1,49 @@
 export default function LoginForm() {
+    const formStyle = {
+        width: '20vw',
+        border: '1px solid black',
+        padding: '10px',
+        display: 'grid',
+        gridTemplateColumns: '1fr 4fr ',
+        gridGap: '5px',
+        backgroundColor: '#f2f2f2',
+        borderRadius: '5px'
+    };
+
+    const labelStyle = {
+        textAlign: 'left' as const,
+        fontWeight: 'bold'
+    };
+
+    const inputStyle = {
+        padding: '5px',
+        border: '2px solid #ccc',
+        borderRadius: '5px',
+        width: '100%',
+        boxSizing: 'border-box' as const
+    }
+
+    const submitStyle = {
+        backgroundColor: '#4CAF50',
+        color: 'white',
+        padding: '10px 20px',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+        justifySelf: 'center',
+        alignSelf: 'center',
+        gridColumn: '1 / span 2'
+    }
+
     return (
-        <form>
-            <label>
-                Username:
-                <input type='text' name='username'></input>
-            </label>
-            <label>
-                <input type='password' name='password'></input>
-            </label>
+        <form style={formStyle}>
+                <label style={labelStyle} htmlFor='username'>Username:</label>
+                <input style={inputStyle} type='text' name='username' id='username'></input>
+
+                <label style={labelStyle} htmlFor='password'>Password:</label>
+                <input style={inputStyle} type='password' name='password' id='password'></input>
+
+                <input style={submitStyle} type='submit' value='Login'></input>
         </form>
     )
 }
