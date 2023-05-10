f0e4f816ac4b56bd05cabc24aae81f483d8bbf74
Radu <raduadrian.constantin94@gmail.com>
2023-05-10 09:31:53 +0300

    Added React Router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,12 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Login from './routes/login';
+import {
+  createBrowserRouter, RouterProvider
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Placeholder!</div>
+  },
+  {
+    path: "/login",
+    element: <Login />
+  }
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
