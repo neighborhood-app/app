e7af0aba6f048dfd4a7a4a0309b658b594bf545d
Radu <raduadrian.constantin94@gmail.com>
2023-05-16 19:58:53 +0300

    Updated LoginForm tests

--- frontend/src/components/LoginForm/LoginForm.test.tsx
+++ frontend/src/components/LoginForm/LoginForm.test.tsx
@@ -3,35 +3,24 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import LoginForm from './LoginForm';
 import loginService from '../../services/login'; 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from '../../routes/login/login';
 
 // Use screen.debug(element) if you want to check out the rendered HTML;
 
 test('LoginForm is rendered', async () => {
-  render(<LoginForm />);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LoginForm />,
+    },
+  ])
+
+  render(<RouterProvider router={router} />);
 
   const form = screen.getByRole('form');
   const usernameInput = screen.getByLabelText(/^username/i);
   const passwordInput = screen.getByLabelText(/^password/i);
-
-  expect(form).toBeDefined();
-  expect(usernameInput).toBeDefined();
-  expect(passwordInput).toBeDefined();
-});
-
-test('Login service is called when form is submitted', async () => {
-  render(<LoginForm />);
-
-  const spyLogin = jest.spyOn(loginService, 'login');
-
-  const form = screen.getByRole('form');
-  const usernameInput = screen.getByLabelText(/^username/i);
-  const passwordInput = screen.getByLabelText(/^password/i);
-
-  const user = userEvent.setup();
-  const button = screen.getByRole('button');
-
-  await user.click(button);
-  expect(spyLogin).toBeCalled();
 
   expect(form).toBeDefined();
   expect(usernameInput).toBeDefined();
