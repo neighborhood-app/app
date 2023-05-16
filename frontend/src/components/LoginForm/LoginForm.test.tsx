7675611ed3ecae2b03fc4a7d9b055a512da9c0ae
Radu <raduadrian.constantin94@gmail.com>
2023-05-16 19:59:21 +0300

    Removed unnecessary dependencies from LoginForm.test.tsx

--- frontend/src/components/LoginForm/LoginForm.test.tsx
+++ frontend/src/components/LoginForm/LoginForm.test.tsx
@@ -1,10 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import LoginForm from './LoginForm';
-import loginService from '../../services/login'; 
+import LoginForm from './LoginForm'; 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Login from '../../routes/login/login';
 
 // Use screen.debug(element) if you want to check out the rendered HTML;
 
