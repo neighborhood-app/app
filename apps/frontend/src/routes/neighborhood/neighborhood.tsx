f3016b3465bd4d4ba844437519b696e6f126d61f
Radu <raduadrian.constantin94@gmail.com>
2023-06-18 11:34:48 +0300

    Experimenting with CSS grid

--- apps/frontend/src/routes/neighborhood/neighborhood.tsx
+++ apps/frontend/src/routes/neighborhood/neighborhood.tsx
@@ -2,33 +2,14 @@ import { Container, Row, Col, Card } from "react-bootstrap"
 
 export default function Neighborhood() {
   return (
-    <Container>
-      <Row>
-        <h1 style={{textAlign: 'center'}}>NEIGHBORHOOD</h1>
-      </Row>
-      <Row>
-        <Col>
-          <Card>
-            <Card.Body>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non eius ipsam asperiores voluptate dolore magnam, laborum, illum repudiandae assumenda suscipit, ab officiis iste facilis velit a magni quam dolor perferendis commodi nesciunt sint eum! Mollitia libero, nemo ab corporis optio numquam minus doloremque laudantium quos laboriosam sunt tempore ducimus enim voluptate dolorum, provident voluptatem sapiente esse architecto quae cum quod error reprehenderit! Suscipit iste nesciunt quisquam molestiae saepe error minima magnam maxime numquam, atque cum, ab nam ipsam quia natus dolorem beatae at quos. Deserunt adipisci vero inventore nulla illo corporis tenetur atque totam, iste, suscipit perspiciatis ad itaque velit!
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col lg={7}>
-          <Card>
-            <Card.Body>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non eius ipsam asperiores voluptate dolore magnam, laborum, illum repudiandae assumenda suscipit, ab officiis iste facilis velit a magni quam dolor perferendis commodi nesciunt sint eum! Mollitia libero, nemo ab corporis optio numquam minus doloremque laudantium quos laboriosam sunt tempore ducimus enim voluptate dolorum, provident voluptatem sapiente esse architecto quae cum quod error reprehenderit! Suscipit iste nesciunt quisquam molestiae saepe error minima magnam maxime numquam, atque cum, ab nam ipsam quia natus dolorem beatae at quos. Deserunt adipisci vero inventore nulla illo corporis tenetur atque totam, iste, suscipit perspiciatis ad itaque velit!
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-        <Card>
-            <Card.Body>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non eius ipsam asperiores voluptate dolore magnam, laborum, illum repudiandae assumenda suscipit, ab officiis iste facilis velit a magni quam dolor perferendis commodi nesciunt sint eum! Mollitia libero, nemo ab corporis optio numquam minus doloremque laudantium quos laboriosam sunt tempore ducimus enim voluptate dolorum, provident voluptatem sapiente esse architecto quae cum quod error reprehenderit! Suscipit iste nesciunt quisquam molestiae saepe error minima magnam maxime numquam, atque cum, ab nam ipsam quia natus dolorem beatae at quos. Deserunt adipisci vero inventore nulla illo corporis tenetur atque totam, iste, suscipit perspiciatis ad itaque velit!
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    <div className="container"
+      style= {{
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr 1fr',
+        gridTemplateRows: '1fr 1fr 1fr 1fr'
+      }}
+    >
+
+    </div>
   )
 }
