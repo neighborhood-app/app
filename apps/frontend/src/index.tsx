e582503be1dde9f89c71721f1fe46132067cf2e8
Radu <raduadrian.constantin94@gmail.com>
2023-07-11 20:42:46 +0300

    Deleted app.tsx and app css

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -10,7 +10,7 @@ import {
 } from 'react-router-dom';
 import MainLayout from './components/MainLayout/MainLayout';
 import Neighborhoods, {loader as neighborhoodsLoader} from './routes/neighborhoods/neighborhoods';
-import Neighborhood, {loader as neighborhoodLoader} from './routes/neighborhood/neighborhood';
+import Neighborhood, {loader as neighborhoodLoader} from './routes/individual_neighborhood/neighborhood';
 
 const router = createBrowserRouter([
   {
