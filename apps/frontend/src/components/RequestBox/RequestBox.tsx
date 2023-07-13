9b20f6733ad3c5bc929b6210255a22438d1803bb
Radu Constantin <raduadrian.constantin94@gmail.com>
2023-07-13 14:37:14 +0300

    Fix

--- apps/frontend/src/components/RequestBox/RequestBox.tsx
+++ apps/frontend/src/components/RequestBox/RequestBox.tsx
@@ -4,19 +4,20 @@ import { Button } from "react-bootstrap";
 
 //@ts-ignore
 export default function RequestBox({ requests}) {
-  console.log(requests);
   //@ts-ignore
   const requestBoxes = requests.map(request => {
-    return (
-      <div className={styles.requestBox}>
-        <div className={styles.requestHeader}>
-          <p className={styles.author}>{request.user.user_name}</p>
-          <p>{request.time_created}</p>
+    const date = request.time_created.split('T')[0];
+
+      return (
+        <div className={styles.requestBox}>
+          <div className={styles.requestHeader}>
+            <p className={styles.author}>{request.user.user_name}</p>
+            <p>{date}</p>
+          </div>
+          <p>{request.title}</p>
+          <Button className={styles.button}>Show Details</Button>
         </div>
-        <p>{request.content}</p>
-        <Button className={styles.helpButton}>Offer help</Button>
-      </div>
-    )
+      )
   })
 
   return (
