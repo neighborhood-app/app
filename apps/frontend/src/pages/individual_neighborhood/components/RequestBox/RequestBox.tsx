1d10198f41ffcea29a8dee0ee74c6ea3d6988973
Radu Constantin <raduadrian.constantin94@gmail.com>
2023-07-25 10:38:05 +0300

    Rough draft of create request modal

--- apps/frontend/src/pages/individual_neighborhood/components/RequestBox/RequestBox.tsx
+++ apps/frontend/src/pages/individual_neighborhood/components/RequestBox/RequestBox.tsx
@@ -1,11 +1,16 @@
 import SearchFilterForm from "../SearchFilterForm/SearchFilterForm";
 import Request from "../Request/Request";
 import styles from './RequestBox.module.css';
-import { Button } from "react-bootstrap";
+import { Button, Modal, Form } from "react-bootstrap";
 import { useState } from "react";
 
-//@ts-ignore
+// @ts-ignore
 export default function RequestBox({ requests }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   const [requestsType, setRequestsType] = useState('open');
   let requestSelection;
   if (requestsType === 'open') {
@@ -31,7 +36,36 @@ export default function RequestBox({ requests }) {
   return (
     <div className={styles.column}>
       <SearchFilterForm filterStatus={ requestsType } setFilterStatus={ setRequestsType }/>
-      <Button className={styles.button}>Create request</Button>
+      <Button className={styles.button} onClick={handleShow}>Create request</Button>
+      <Modal show={show} onHide={handleClose} animation={true} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Create a request</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formTitle">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="text" placeholder="Title of your request" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formContent">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="text" placeholder="Content of your request" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
       {requestBoxes}
     </div>
   )
