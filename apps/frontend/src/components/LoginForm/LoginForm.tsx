d908ff4a14378903bcff0a7494d5742db208dbd8
nina-dk <antwnina.k@gmail.com>
2023-08-22 13:25:09 +0300

    add bootstrap form/buttons

--- apps/frontend/src/components/LoginForm/LoginForm.tsx
+++ apps/frontend/src/components/LoginForm/LoginForm.tsx
@@ -1,21 +1,43 @@
 import styles from './LoginForm.module.css'
-import { Form } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
 
 export default function LoginForm() {
     return (
         <div className={styles.formContainer}>
-            <div className={styles.logoContainer}>
-                <h2>NEIGHBORHOOD</h2>
-            </div>
-            <Form method="post" className={styles.form} name='login-form'>
-                <label className={styles.label} htmlFor='username'>USERNAME:</label>
+          <div className={styles.logoContainer}>
+            <h2>Log In</h2>
+        </div>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicUsername">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text" placeholder="Your username" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <div className="d-grid gap-2">
+            <Button className={styles.mainBtn} variant="primary" type="submit">
+              Submit
+            </Button>
+            <Button variant="outline-dark" href='#'>
+              Don't have an account? Sign up!
+            </Button>
+          </div>
+        </Form>
+            {/* <Form method="post" className={styles.form} name='login-form'>
+                <label className={styles.label} htmlFor='username'>Username</label>
                 <input className={styles.input} type='text' name='username' id='username' required></input>
 
-                <label className={styles.label} htmlFor='password'>PASSWORD:</label>
+                <label className={styles.label} htmlFor='password'>Password</label>
                 <input className={styles.input} type='password' name='password' id='password' required></input>
 
-                <input className={styles.submit} type='submit' value='LOGIN'></input>
-            </Form>
+                <input className={styles.submit} type='submit' value='LOGIN'></input> */}
+            {/* </Form> */}
         </div>
     )
 }
