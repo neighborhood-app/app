aa079ff770e3ca4ef18f0ee800d19cdea1a534f3
nina-dk <antwnina.k@gmail.com>
2023-09-01 21:03:49 +0300

    css changes

--- apps/frontend/src/components/LoginForm/LoginForm.tsx
+++ apps/frontend/src/components/LoginForm/LoginForm.tsx
@@ -1,6 +1,6 @@
 import styles from './LoginForm.module.css'
 import { Form, Button, Container, Col, Row, Image } from 'react-bootstrap';
-import { Form as FormRouter } from 'react-router-dom';
+import { Form as FormRouter, Link } from 'react-router-dom';
 
 export default function LoginForm() {
     return (
@@ -13,31 +13,30 @@ export default function LoginForm() {
           <h2 className={styles.welcomeHeading}>Welcome!</h2>
           <p>Connect with your neighbors. Share resources. Build a strong community.</p>
         </Col>
-        <Col className={styles.customCol}>
-          <Container>
-            <h2>Log In</h2>
-            <FormRouter method='post'>
-              <Form.Group className='mb-3' controlId='username'>
-                <Form.Label>Username</Form.Label>
-                <Form.Control type='text' name='username' placeholder='Your username' required />
-              </Form.Group>
-              <Form.Group className='mb-3' controlId='password'>
-                <Form.Label>Password</Form.Label>
-                <Form.Control type='password' name='password' placeholder='Password' required />
-              </Form.Group>
-              <Form.Group className='mb-3' controlId='formBasicCheckbox'>
-                <Form.Check type='checkbox' label='Remember me' />
-              </Form.Group>
-              <div className='d-grid gap-2'>
-                <Button className={styles.mainBtn} variant='primary' type='submit'>
-                  Submit
-                </Button>
-                <Button variant='outline-dark' type='button' href='#'>
-                  Don't have an account? Sign up!
-                </Button>
-              </div>
-            </FormRouter>
-          </Container>
+        <Col className={`${styles.customCol} ${styles.formWrapper}`}>
+          <h2>Log In</h2>
+          <FormRouter method='post'>
+            <Form.Group className='mb-3' controlId='username'>
+              <Form.Label>Username</Form.Label>
+              <Form.Control type='text' name='username' placeholder='Your username' required />
+            </Form.Group>
+            <Form.Group className='mb-3' controlId='password'>
+              <Form.Label>Password</Form.Label>
+              <Form.Control type='password' name='password' placeholder='Password' required />
+            </Form.Group>
+            <Form.Group className={`mb-3 ${styles.checkboxLinkWrapper}`} controlId='formBasicCheckbox'>
+              <Form.Check type='checkbox' label='Remember me' />
+              <Link to={"#"}>Forgot your password?</Link>
+            </Form.Group>
+            <div className='d-grid gap-2'>
+              <Button className={styles.mainBtn} variant='primary' type='submit'>
+                Submit
+              </Button>
+              <Button variant='outline-dark' type='button' href='#'>
+                Don't have an account? Sign up!
+              </Button>
+            </div>
+          </FormRouter>
         </Col>
         </Row>
       </Container>
