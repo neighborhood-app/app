1ea7b5d22f336a1f3567d6112dcec52728f128a9
nina-dk <antwnina.k@gmail.com>
2023-09-04 13:00:21 +0300

    narrow form

--- apps/frontend/src/components/LoginForm/LoginForm.tsx
+++ apps/frontend/src/components/LoginForm/LoginForm.tsx
@@ -1,12 +1,12 @@
 import styles from './LoginForm.module.css'
-import { Form, Button, Col } from 'react-bootstrap';
+import { Form, Button, Col, Container } from 'react-bootstrap';
 import { Form as FormRouter, Link } from 'react-router-dom';
 
 export default function LoginForm({ className }: {className: string}) {
     return (
-      <Col className={className}>
-        <h2>Log In</h2>
+      <Col lg={6} className={className}>
         <FormRouter method='post' role='form'>
+          <h2>Log In</h2>
           <Form.Group className='mb-3' controlId='username'>
             <Form.Label>Username</Form.Label>
             <Form.Control type='text' name='username' placeholder='Your username' required />
@@ -16,8 +16,14 @@ export default function LoginForm({ className }: {className: string}) {
             <Form.Control type='password' name='password' placeholder='Password' required />
           </Form.Group>
           <Form.Group className={`mb-3 ${styles.checkboxLinkWrapper}`} controlId='formBasicCheckbox'>
-            <Form.Check type='checkbox' label='Remember me' />
-            <Link to={"#"}>Forgot your password?</Link>
+            <Container className={styles.checkboxLinkWrapper}>
+              <Col><Form.Check type='checkbox' label='Remember me' /></Col>
+              <Col>
+                <Link to={"#"}>Forgot your password?</Link>
+              </Col>
+            </Container>
+            {/* <Form.Check type='checkbox' label='Remember me' /> */}
+            {/* <Link to={"#"}>Forgot your password?</Link> */}
           </Form.Group>
           <div className='d-grid gap-2'>
             <Button className={styles.mainBtn} variant='primary' type='submit'>
--- apps/frontend/src/components/WelcomeImgBox/WelcomeImgBox.tsx
+++ apps/frontend/src/components/WelcomeImgBox/WelcomeImgBox.tsx
@@ -3,7 +3,7 @@ import { Col, Image } from 'react-bootstrap';
 
 export default function WelcomeImgBox({ className }: {className: string}) {
   return (
-    <Col md className={className}>
+    <Col lg={6} className={className}>
       <Image className={styles.neighborhoodImg} src={require('../../assets/neighborhood2.jpeg')} rounded fluid></Image>
       <h2 className={styles.welcomeHeading}>Welcome!</h2>
       <p>Connect with your neighbors. Share resources. Build a strong community.</p>
