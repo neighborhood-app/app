53b199128ebbda744f35cb560e4df5f719ec6072
Radu <raduadrian.constantin94@gmail.com>
2023-09-16 09:32:15 +0300

    Fixed response box CSS

--- apps/frontend/src/components/Response/Response.tsx
+++ apps/frontend/src/components/Response/Response.tsx
@@ -3,10 +3,11 @@ import styles from './Response.module.css';
 export default function Response() {
   return (
     <div className={styles.responseCard}>
-      <div className={styles.profieAndDate}>
+      <div className={styles.profileAndDate}>
         <div className={styles.profileInfo}>
           <img
-            src="images/profile-2.jpg"
+            className={styles.profileImg}
+            src={require('./images/profile.jpg')}
             alt="active user on neighborhood app" />
           <p className={styles.p}>Jane Parker</p>
         </div>
@@ -16,7 +17,7 @@ export default function Response() {
         I can help out with your drowning cat. I've been a lifeguard for 5
         years and I know how to save a cat.
       </p>
-      <hr />
+      <hr className={styles.hr}/>
       <div>
         <p className={styles.p}>You've accepted this offer for help.</p>
         <p className={styles.p}>Contact at: <span>jane.parker@gmail.com</span></p>
