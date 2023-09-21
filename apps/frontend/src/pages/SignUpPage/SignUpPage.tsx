8748f985b17ac8a06dcb1f4298fbd4167e05a6b5
nina-dk <antwnina.k@gmail.com>
2023-09-21 12:58:14 +0300

    minor css changes

--- apps/frontend/src/pages/SignUpPage/SignUpPage.tsx
+++ apps/frontend/src/pages/SignUpPage/SignUpPage.tsx
@@ -17,11 +17,11 @@ export async function action({ request }: { request: Request }) {
 export default function SignUpPage() {
   return (
     <Container className={styles.wrapperContainer} fluid>
-    <h1 className={styles.neighborhoodHeading}>Neighborhood</h1>
-    <Row className={styles.customRow}>
-      <WelcomeImgBox className={`${styles.customCol} ${styles.imgCol}`}></WelcomeImgBox>
-      <SignUpForm className={`${styles.customCol} ${styles.formWrapper}`}></SignUpForm>
-    </Row>
+      <h1 className={styles.neighborhoodHeading}>Neighborhood</h1>
+      <Row className={styles.customRow}>
+        <WelcomeImgBox className={`${styles.customCol} ${styles.imgCol}`}></WelcomeImgBox>
+        <SignUpForm className={`${styles.customCol} ${styles.formWrapper}`}></SignUpForm>
+      </Row>
   </Container>
   );
 }
