ab69f21a8268f976d3737783667473a03fe00f02
nina-dk <antwnina.k@gmail.com>
2023-10-03 15:48:40 +0300

    Document CustomBtn component

--- apps/frontend/src/components/CustomBtn/CustomBtn.tsx
+++ apps/frontend/src/components/CustomBtn/CustomBtn.tsx
@@ -4,6 +4,11 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant: 'primary' | 'outline-dark' | 'danger';
 };
 
+/**
+ * @param variant - one of 'primary', 'outline-dark' or 'danger'
+ * @param - any other valid Button attributes
+ * @returns a styled Button component according to the variant attribute
+ */
 export default function CustomBtn({ ...props }: ButtonProps) {
   return (
     <Button {...props}></Button>
