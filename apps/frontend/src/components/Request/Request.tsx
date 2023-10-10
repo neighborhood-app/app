3048f83c913c4fc488a19befc10f9aae97c15466
Radu <raduadrian.constantin94@gmail.com>
2023-10-10 21:37:55 +0300

    Added flag for closed requests

--- apps/frontend/src/components/Request/Request.tsx
+++ apps/frontend/src/components/Request/Request.tsx
@@ -3,26 +3,33 @@ import { RequestType } from "../../types";
 import { useState } from "react";
 import RequestModal from "../RequestModal/RequestModal";
 
-export default function Request({ requestObj }: { requestObj: RequestType}) {
+export default function Request({ requestObj }: { requestObj: RequestType }) {
   const date = requestObj.time_created.split("T")[0];
   const [show, setShow] = useState(false);
 
   const handleCloseModal = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const closedHeader = (
+    <div className={styles.dividerVertical}>
+      <h2 className={styles.closedHeader}>CLOSED</h2>
+    </div>
+  )
+
   return (
     <div className={styles.container} onClick={handleShow}>
       <div className={styles.requestHeader}>
         <p>{requestObj.user.username}</p>
       </div>
-      <div className={styles.requestImage}>
+      <div className={styles.imageContainer}>
         <img className={styles.image} src={require('./help_wanted.jpeg')} alt='Help Wanted' />
+        {requestObj.status === 'CLOSED' ? closedHeader : null}
       </div>
       <div className={styles.requestContent}>
         <p className={styles.title}>{requestObj.title}</p>
         <p className={styles.date}>Created on {date}</p>
       </div>
-      <RequestModal show={show} handleCloseModal={handleCloseModal} request={requestObj}/>
+      <RequestModal show={show} handleCloseModal={handleCloseModal} request={requestObj} />
     </div>
   );
 }
