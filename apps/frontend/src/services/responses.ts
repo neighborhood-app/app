2b241a2b71b2962bca14d7d44d2e598b9251d9b7
Radu Constantin <raduadrian.constantin94@gmail.com>
2023-10-12 13:13:08 +0300

    Using getStoredUser helper function to retrieve logged user in services/responses.ts

--- apps/frontend/src/services/responses.ts
+++ apps/frontend/src/services/responses.ts
@@ -1,15 +1,15 @@
+/* eslint-disable import/no-anonymous-default-export */
 import axios from "axios";
-import { StorageWithUser, UserInfo } from "../types";
+import { getStoredUser } from "../utils/auth";
 
 const baseURL = '/api/responses';
+let user = getStoredUser();
 
 async function acceptResponse(responseId: string) {
   const headers: { authorization?: string } = {};
-  let { user }: { user?: string } = localStorage as StorageWithUser;
 
   if (user) {
-    const userObj: UserInfo = JSON.parse(user);
-    headers.authorization = `Bearer ${userObj.token}`;
+    headers.authorization = `Bearer ${user.token}`;
   }
 
   const response = await axios.put(`${baseURL}/${responseId}`, { status: 'ACCEPTED' }, { headers });
@@ -19,11 +19,9 @@ async function acceptResponse(responseId: string) {
 
 async function deleteResponse(responseId: string) {
   const headers: { authorization?: string } = {};
-  let { user }: { user?: string } = localStorage as StorageWithUser;
 
   if (user) {
-    const userObj: UserInfo = JSON.parse(user);
-    headers.authorization = `Bearer ${userObj.token}`;
+    headers.authorization = `Bearer ${user.token}`;
   }
 
   const response = await axios.delete(`${baseURL}/${responseId}`, { headers });
