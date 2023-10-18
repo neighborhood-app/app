104753f8920431660444c91c29eb9bc15c48ff73
Radu Constantin <raduadrian.constantin94@gmail.com>
2023-10-18 06:54:11 +0300

    Integrated UserCircleStack into neighborhood page

--- apps/frontend/src/components/DescriptionBox/DescriptionBox.tsx
+++ apps/frontend/src/components/DescriptionBox/DescriptionBox.tsx
@@ -1,32 +1,54 @@
-import CustomBtn from '../CustomBtn/CustomBtn';
-import styles from './DescriptionBox.module.css';
-import { User } from '@prisma/client';
-import { JoinNeighborhoodForm } from '../JoinNeighborhoodForm/JoinNeighborhoodForm';
+import CustomBtn from "../CustomBtn/CustomBtn";
+import styles from "./DescriptionBox.module.css";
+import { User } from "@prisma/client";
+import { JoinNeighborhoodForm } from "../JoinNeighborhoodForm/JoinNeighborhoodForm";
+import UserCircleStack from "../UserCircleStack/UserCircleStack";
 
 interface Props {
-  showJoinBtn: boolean,
-  showEditBtn: boolean,
-  showLeaveBtn: boolean,
-  name: string,
-  description: string,
-  users?: Array<User> | null
+  showJoinBtn: boolean;
+  showEditBtn: boolean;
+  showLeaveBtn: boolean;
+  name: string;
+  description: string;
+  users?: Array<User> | null;
 }
 
-export default function DescriptionBox({showJoinBtn, showEditBtn, showLeaveBtn, name, description, users}: Props) {
+export default function DescriptionBox({
+  showJoinBtn,
+  showEditBtn,
+  showLeaveBtn,
+  name,
+  description,
+  users,
+}: Props) {
   return (
     <div className={styles.container}>
-      <div className={styles.card}>
-        <img className={styles.neighborhoodImg} src={require('./palm.jpeg')} alt='Neighborhood'/>
-        <h1 className={styles.neighborhoodTitle}>{name}</h1>
-        {showJoinBtn ? <JoinNeighborhoodForm></JoinNeighborhoodForm> : null}
+      <div className={styles.firstHalf}>
+        <div className={styles.card}>
+          <img
+            className={styles.neighborhoodImg}
+            src={require("./palm.jpeg")}
+            alt="Neighborhood"
+          />
+          <h1 className={styles.neighborhoodTitle}>{name}</h1>
+          {showJoinBtn ? <JoinNeighborhoodForm></JoinNeighborhoodForm> : null}
+        </div>
+        <div className={styles.neighborhoodDescription}>
+          <p>{description}</p>
+          {users ? <p>{users.length} members</p> : null}
+          {showEditBtn ? (
+            <CustomBtn variant="outline-dark" className={styles.editBtn}>
+              Edit Neighborhood
+            </CustomBtn>
+          ) : null}
+          {showLeaveBtn ? (
+            <CustomBtn variant="danger">Leave Neighborhood</CustomBtn>
+          ) : null}
+        </div>
       </div>
-      <div className={styles.neighborhoodDescription}>
-        <p>{description}</p>
-        {users ? <p>{users.length} members</p> : null}
-        {showEditBtn ? <CustomBtn variant='outline-dark' className={styles.editBtn}>Edit Neighborhood</CustomBtn> : null}
-        {showLeaveBtn ? <CustomBtn variant='danger'>Leave Neighborhood</CustomBtn> : null}
+      <div className={styles.secondHalf}>
+        <UserCircleStack usernames={["mike", "antonina", "maria", "radu"]} />
       </div>
     </div>
-  )
-};
-
+  );
+}
---
+++ apps/frontend/src/components/UserCircleStack/UserCircleStack.jsx
@@ -0,0 +1,23 @@
+import styles from "./UserCircleStack.module.css";
+
+export default function UserCircleStack({ usernames }) {
+  const displayUsers = usernames.slice(0, 4);
+
+  return (
+    <div className={styles.circleContainer}>
+      {displayUsers.map((username) => (
+        <UserCircle key={username} username={username} />
+      ))}
+    </div>
+  );
+}
+
+function UserCircle({ username }) {
+  const firstTwoLetters = username.slice(0, 2).toUpperCase();
+
+  return (
+    <div className={styles.circle} style={{ style: "--i: 1" }}>
+      {firstTwoLetters}
+    </div>
+  );
+}
