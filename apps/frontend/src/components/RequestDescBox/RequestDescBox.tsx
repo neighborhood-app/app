0a96cbbaa897b76d6f352e936bd9a821f08c874e
nina-dk <antwnina.k@gmail.com>
2023-11-06 20:52:02 +0200

    Add request action buttons; style Offer Help btn

--- apps/frontend/src/components/RequestDescBox/RequestDescBox.tsx
+++ apps/frontend/src/components/RequestDescBox/RequestDescBox.tsx
@@ -1,12 +1,15 @@
 // import { useParams } from 'react-router';
 // import CustomBtn from '../CustomBtn/CustomBtn';
 // import TriggerActionButton from '../TriggerActionButton/TriggerActionButton';
+// import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Col, Container, Image, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 import styles from './RequestDescBox.module.css';
-import { FullRequestData } from '../../types';
+import { FullRequestData, StoredUserData } from '../../types';
+import { getStoredUser } from '../../utils/auth';
+import CustomBtn from '../CustomBtn/CustomBtn';
 
 const requestImg = require('../../assets/help_wanted.jpeg');
 
@@ -22,27 +25,99 @@ export default function RequestDescBox({ request }: Props) {
   if (user.first_name && user.last_name) userName = `${user.first_name} ${user.last_name}`;
   else userName = user.username;
 
+  // const [loading, setIsLoading] = useState(false);
+  // setIsLoading(true);
+  // const [responseInput, setResponseInput] = useState('');
+
+  const loggedUser = getStoredUser() as StoredUserData;
+  const { username, id: loggedUserId } = loggedUser;
+
+  function handleCloseRequest() {
+    console.log('closing');
+
+    // try {
+    //   setIsLoading(true);
+    //   await requestServices.closeRequest(String(request.id));
+    //   setIsLoading(false);
+    //   handleCloseModal();
+    //   revalidator.revalidate();
+    // } catch (e) {
+    //   setIsLoading(false);
+    //   console.log(e);
+    // }
+  }
+
+  function handleDeleteRequest() {
+    console.log('deleting');
+
+    // await requestServices.deleteRequest(String(request.id));
+    // handleCloseModal();
+    // revalidator.revalidate();
+  }
+
+  const hasUserResponded = request.responses.some(
+    (response) => response.user_id === Number(loggedUserId),
+  );
+
+  /**
+  * Based on the status of a user (creator, or viewer) and the status of the request 
+  (OPEN or CLOSED) returns the corresponding JSX for the request modal.
+  */
+  const displayRequestActions = () => {
+    if (username === request.user.username && request.status === 'OPEN') {
+      return (
+        <>
+          <CustomBtn variant="primary" onClick={handleCloseRequest}>
+            Close request
+          </CustomBtn>
+          <CustomBtn variant="danger" onClick={handleDeleteRequest}>
+            Delete request
+          </CustomBtn>
+        </>
+      );
+    }
+    if (request.status === 'OPEN' && !hasUserResponded) {
+      return (
+        <Col sm="2" xs="4" className="pe-0 me-md-5">
+          <CustomBtn
+            className={styles.actionBtn}
+            variant="primary"
+            onClick={() => console.log('Helping!')}>
+            Offer help
+          </CustomBtn>
+        </Col>
+      );
+    }
+
+    return null;
+  };
+
   return (
     <Container className="m-sm-3 mt-4" fluid>
-      <Row className='mb-2 mt-sm-4'>
+      <Row className="mb-2 mt-sm-4">
         <Link to={`/neighborhoods/${neighborhood.id}`}>
           <FontAwesomeIcon className={styles.backIcon} icon={faAngleLeft} />
           <h5 className={styles.neighborhoodTitle}>{neighborhood.name}</h5>
         </Link>
       </Row>
-      <Row>
+      <Row className="gx-3">
         <Col sm="3">
-          <Image fluid className={styles.requestImg} rounded src={requestImg} alt="Request" />
+          <Image className={styles.requestImg} rounded src={requestImg} alt="Request" />
           <p className={`${styles.requestDate} small`}>Created at {requestDate}</p>
         </Col>
-        <Col className='me-sm-2'>
+        <Col className="me-sm-2">
           <h3>{request.title}</h3>
           <Link to="#">
-            <Image fluid className={styles.userIcon} roundedCircle src={requestImg} alt="Request" />
+            <Image className={styles.userIcon} roundedCircle src={requestImg} alt="Request" />
             <p className={`${styles.userName} text-muted`}>{userName}</p>
           </Link>
           <p>{request.content}</p>
         </Col>
+      </Row>
+      <Row className="mb-3 gx-3 justify-content-sm-start justify-content-center">
+        <Col sm="3"></Col>
+        {displayRequestActions()}
+        {/* {loading ? <Spinner animation="border" /> : null} */}
       </Row>
     </Container>
   );
