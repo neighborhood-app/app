9f7623537417624ea8275fa4283fe09035ed8087
Radu Constantin <raduadrian.constantin94@gmail.com>
2023-11-15 09:21:42 +0200

    Neighborhood card shows if user is admin

--- apps/frontend/src/components/NeighborhoodCard/NeighborhoodCard.tsx
+++ apps/frontend/src/components/NeighborhoodCard/NeighborhoodCard.tsx
@@ -6,16 +6,18 @@ const neighborhoodImg = require('./images/palm.jpeg');
 type Props = {
   name: string;
   description: string | null;
+  isUserAdmin: boolean;
 };
 
-export default function NeighborhoodsBox({ name, description }: Props) {
+export default function NeighborhoodsBox({ name, description, isUserAdmin }: Props) {
+
   return (
     <Figure className={styles.card}>
       <Figure.Image src={neighborhoodImg} alt="neighborhood" className={styles.image} />
       <Figure.Caption className={styles.figcaption}>
         <span className={styles.info}>
           <h2>
-            {name} <span>(Admin)</span>
+            {name} <span>{isUserAdmin ? 'Admin' : null}</span>
           </h2>
           <p>{description}</p>
         </span>
