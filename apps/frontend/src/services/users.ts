0ca73bc03f3274928844a26549abed0a19fd6ebf
Radu Constantin <raduadrian.constantin94@gmail.com>
2023-11-22 06:53:49 +0200

    Using getStoredUser in users.ts services

--- apps/frontend/src/services/users.ts
+++ apps/frontend/src/services/users.ts
@@ -1,16 +1,15 @@
 import axios from 'axios';
 import { UserWithRelatedData } from '@neighborhood/backend/src/types';
-import { StorageWithUser, UserInfo } from '../types';
+import { getStoredUser } from '../utils/auth';
 
 const baseURL = '/api/users';
 
 async function getUserData(id: number): Promise<UserWithRelatedData> {
   const headers: { authorization?: string } = {};
-  const { user }: { user?: string } = localStorage as StorageWithUser;
+  const user = getStoredUser();
 
   if (user) {
-    const userObj: UserInfo = JSON.parse(user);
-    headers.authorization = `Bearer ${userObj.token}`;
+    headers.authorization = `Bearer ${user.token}`;
   }
 
   const response = await axios.get(`${baseURL}/${id}`, { headers });
