9699dc5c18068bf0a741e5cfbc8aa1faaf366564
Radu Constantin <raduadrian.constantin94@gmail.com>
2023-11-27 06:57:21 +0200

    Added profile picture and edit button

--- apps/frontend/src/pages/ProfilePage/ProfilePage.tsx
+++ apps/frontend/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,5 +1,9 @@
 import { LoaderFunctionArgs, useLoaderData } from 'react-router';
-// import { getStoredUser } from '../../utils/auth';
+import { UserWithoutPasswordHash } from '@neighborhood/backend/src/types';
+import { Container, Row, Col } from 'react-bootstrap';
+import CustomBtn from '../../components/CustomBtn/CustomBtn';
+import styles from './ProfilePage.module.css';
+import { getStoredUser } from '../../utils/auth';
 import usersServices from '../../services/users';
 
 export async function loader({ params }: LoaderFunctionArgs) {
@@ -8,10 +12,32 @@ export async function loader({ params }: LoaderFunctionArgs) {
   return userData;
 }
 
-export default function ProfilePage() {
-  // const loggedUser = getStoredUser();
-  const user = useLoaderData();
-  console.log(user);
+const profileImage = require('./profile-picture.png');
 
-  return <p>Test</p>;
+export default function ProfilePage() {
+  const loggedUser = getStoredUser();
+  const profileData = useLoaderData() as UserWithoutPasswordHash;
+
+  let isUserAdmin: boolean | null;
+  if (loggedUser) {
+    isUserAdmin = loggedUser.id === profileData.id;
+  } else {
+    isUserAdmin = null;
+  }
+
+  console.log(isUserAdmin);
+
+  return (
+    <Container className={styles.container}>
+      <Row className={styles.header}>
+        <Col className={styles.column}>
+          <img src={profileImage} alt="Profile" className={styles.profilePicture} />
+        </Col>
+        <Col className={styles.column}>
+          <h3 className={styles.userName}>{profileData.username}</h3>
+          <CustomBtn variant="primary">Edit Profile</CustomBtn>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
