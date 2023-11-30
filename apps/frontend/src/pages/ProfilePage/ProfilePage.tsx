771ee1363f6a6ab11f02a086892867c63a5bde13
Radu Constantin <raduadrian.constantin94@gmail.com>
2023-11-30 11:23:02 +0200

    Displaying profile page info

--- apps/frontend/src/pages/ProfilePage/ProfilePage.tsx
+++ apps/frontend/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,5 +1,5 @@
 import { LoaderFunctionArgs, useLoaderData } from 'react-router';
-import { UserWithoutPasswordHash } from '@neighborhood/backend/src/types';
+import { UserWithRelatedData } from '@neighborhood/backend/src/types';
 import { Container, Row, Col } from 'react-bootstrap';
 import CustomBtn from '../../components/CustomBtn/CustomBtn';
 import styles from './ProfilePage.module.css';
@@ -16,7 +16,7 @@ const profileImage = require('./profile-picture.png');
 
 export default function ProfilePage() {
   const loggedUser = getStoredUser();
-  const profileData = useLoaderData() as UserWithoutPasswordHash;
+  const profileData = useLoaderData() as UserWithRelatedData;
 
   let isUserAdmin: boolean | null;
   if (loggedUser) {
@@ -25,17 +25,48 @@ export default function ProfilePage() {
     isUserAdmin = null;
   }
 
+  const displayInfo = {
+    name:
+      profileData.first_name || profileData.last_name
+        ? `${profileData.first_name} ${profileData.last_name}`
+        : null,
+    dob: profileData.dob ? String(profileData.dob) : '-',
+    bio: profileData.bio || '-',
+    email: profileData.email,
+  };
+
   console.log(isUserAdmin);
 
   return (
-    <Container className={styles.container}>
+    <Container className={styles.container} fluid>
       <Row className={styles.header}>
         <Col className={styles.column}>
           <img src={profileImage} alt="Profile" className={styles.profilePicture} />
         </Col>
         <Col className={styles.column}>
-          <h3 className={styles.userName}>{profileData.username}</h3>
-          <CustomBtn variant="primary">Edit Profile</CustomBtn>
+          <h2 className={styles.userName}>{profileData.username}</h2>
+          {displayInfo.name && <p>{displayInfo.name}</p>}
+          <CustomBtn className={styles.btn} variant="primary">
+            Update Profile
+          </CustomBtn>
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={12} md={6}>
+          <h3 className={styles.profileHeader}>Bio</h3>
+          <p className={styles.profileText}>{displayInfo.bio}</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={12} md={6}>
+          <h3>Date of birth:</h3>
+          <p className={styles.profileText}>{displayInfo.dob}</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={12} md={6}>
+          <h3>Email:</h3>
+          <p className={styles.profileText}>{displayInfo.email}</p>
         </Col>
       </Row>
     </Container>
