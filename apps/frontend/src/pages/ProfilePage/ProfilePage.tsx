ba9e4276f7788e59fc1ddd4b177cc8ade61e053b
Radu Constantin <raduadrian.constantin94@gmail.com>
2023-11-30 08:25:31 +0200

    Updated call to usersServices with getUserData

--- apps/frontend/src/pages/ProfilePage/ProfilePage.tsx
+++ apps/frontend/src/pages/ProfilePage/ProfilePage.tsx
@@ -7,8 +7,8 @@ import { getStoredUser } from '../../utils/auth';
 import usersServices from '../../services/users';
 
 export async function loader({ params }: LoaderFunctionArgs) {
-  const userId = params.id as string;
-  const userData = usersServices.getSingleUser(userId);
+  const userId = params.id;
+  const userData = usersServices.getUserData(Number(userId));
   return userData;
 }
 
