31107a0a3f71caa7525fb4d78a509996d99fcb95
Radu Constantin <raduadrian.constantin94@gmail.com>
2023-12-02 10:06:35 +0200

    Added react-date-picker

--- apps/frontend/src/components/EditProfile/EditProfile.tsx
+++ apps/frontend/src/components/EditProfile/EditProfile.tsx
@@ -1,5 +1,7 @@
 import { UserWithRelatedData } from '@neighborhood/backend/src/types';
 import { Col, Row, Form } from 'react-bootstrap';
+import { useState } from 'react';
+import DatePicker from 'react-date-picker';
 import styles from './EditProfile.module.css';
 
 type Props = {
@@ -7,42 +9,64 @@ type Props = {
 };
 
 export default function EditProfile({ profile }: Props) {
-  const displayInfo = {
-    name:
-      profile.first_name || profile.last_name ? `${profile.first_name} ${profile.last_name}` : null,
-    dob: profile.dob ? String(profile.dob) : '-',
-    bio: profile.bio || '-',
-    email: profile.email,
-  };
+  // const displayInfo = {
+  //   name:
+  //     profile.first_name || profile.last_name
+  //       ? `${profile.first_name} ${profile.last_name}`
+  //       : undefined,
+  //   dob: profile.dob ? String(profile.dob) : '-',
+  //   bio: profile.bio || '-',
+  //   email: profile.email,
+  // };
+
+  const [formInput, setFormInput] = useState({
+    firstName: profile.first_name || undefined,
+    lastName: profile.last_name || undefined,
+    bio: profile.bio || undefined,
+    dob: profile.dob || undefined,
+    email: profile.email || undefined,
+  });
+
+  function changeDob(value: Date | undefined) {
+    formInput.dob = value;
+  }
+
+  console.log(formInput, setFormInput);
 
   return (
     <>
-      <Form>
-        <Row>
+      <Form className={styles.form}>
+        <Row className={styles.row}>
           <Col sm={12} md={6}>
+            <h3>First name</h3>
+            <Form.Control value={formInput.firstName} type="text" name="first-name"></Form.Control>
+          </Col>
+          <Col sm={12} md={6}>
+            <h3>Last name</h3>
+            <Form.Control value={formInput.lastName} type="text" name="last-name"></Form.Control>
+          </Col>
+        </Row>
+        <Row className={styles.row}>
+          <Col sm={12} md={12}>
             <h3>Bio</h3>
-            <Form.Control value={displayInfo.bio} as="textarea" rows={6} name="bio"></Form.Control>
+            <Form.Control value={formInput.bio} as="textarea" rows={6} name="bio"></Form.Control>
           </Col>
         </Row>
-        <Row>
+        <Row className={styles.row}>
           <Col sm={12} md={6}>
-            <h3>Date of birth:</h3>
-            <Form.Control
-              className={styles.dobField}
-              type="date"
-              name="dob"
-              value={displayInfo.dob}
-              placeholder={displayInfo.dob}></Form.Control>
+            <h3>Date of birth</h3>
+            {/* <Form.Control className={styles.inputField} type="date" name="dob"></Form.Control> */}
+            <DatePicker
+              className={styles.datePicker}
+              value={formInput.dob}
+              onChange={changeDob}
+              monthPlaceholder="mm"
+              dayPlaceholder="dd"
+            />
           </Col>
-        </Row>
-        <Row>
           <Col sm={12} md={6}>
-            <h3>Email:</h3>
-            <Form.Control
-              className={styles.emailField}
-              value={displayInfo.email}
-              type="text"
-              name="email"></Form.Control>
+            <h3>Email</h3>
+            <Form.Control value={formInput.email} type="text" name="email"></Form.Control>
           </Col>
         </Row>
       </Form>
