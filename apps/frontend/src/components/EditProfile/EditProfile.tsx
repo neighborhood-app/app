09596d1ab3d596d603dab56adbd8f7130a90afc4
Radu Constantin <raduadrian.constantin94@gmail.com>
2023-12-03 18:57:16 +0200

    editProfile is now controlled

--- apps/frontend/src/components/EditProfile/EditProfile.tsx
+++ apps/frontend/src/components/EditProfile/EditProfile.tsx
@@ -29,6 +29,9 @@ export default function EditProfile({ profile, closeForm }: Props) {
             <h3>First name</h3>
             <Form.Control
               value={formInput.firstName}
+              onChange={(event) => {
+                setFormInput((formerInput) => ({ ...formerInput, firstName: event.target.value }));
+              }}
               type="text"
               name="first-name"
               maxLength={25}></Form.Control>
@@ -37,6 +40,9 @@ export default function EditProfile({ profile, closeForm }: Props) {
             <h3>Last name</h3>
             <Form.Control
               value={formInput.lastName}
+              onChange={(event) => {
+                setFormInput((formerInput) => ({ ...formerInput, lastName: event.target.value }));
+              }}
               type="text"
               name="last-name"
               maxLength={25}></Form.Control>
@@ -47,6 +53,9 @@ export default function EditProfile({ profile, closeForm }: Props) {
             <h3>Bio</h3>
             <Form.Control
               value={formInput.bio}
+              onChange={(event) => {
+                setFormInput((formerInput) => ({ ...formerInput, bio: event.target.value }));
+              }}
               as="textarea"
               rows={6}
               name="bio"
@@ -59,6 +68,9 @@ export default function EditProfile({ profile, closeForm }: Props) {
             <Form.Control
               className={styles.inputField}
               value={formInput.dob}
+              onChange={(event) => {
+                setFormInput((formerInput) => ({ ...formerInput, dob: event.target.value }));
+              }}
               type="date"
               name="dob"></Form.Control>
           </Col>
@@ -66,6 +78,9 @@ export default function EditProfile({ profile, closeForm }: Props) {
             <h3>Email</h3>
             <Form.Control
               value={formInput.email}
+              onChange={(event) => {
+                setFormInput((formerInput) => ({ ...formerInput, email: event.target.value }));
+              }}
               type="text"
               name="email"
               maxLength={40}></Form.Control>
