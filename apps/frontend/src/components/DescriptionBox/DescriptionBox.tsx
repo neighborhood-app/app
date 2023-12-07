41c0b5f7dec585c28ed936c2fe57a4ab79f99e9d
Radu Constantin <raduadrian.constantin94@gmail.com>
2023-12-07 19:54:57 +0200

    Adjusted UserCircle font-size and neighborhood description spacing

--- apps/frontend/src/components/DescriptionBox/DescriptionBox.tsx
+++ apps/frontend/src/components/DescriptionBox/DescriptionBox.tsx
@@ -113,7 +113,7 @@ export default function DescriptionBox({
           ) : null}
         </Col>
       </Row>
-      <Row className="mt-2 mt-md-4">
+      <Row className="mt-1 md-4">
         <div className={styles.neighborhoodDescription}>
           {description ? <p>{description}</p> : null}
           {showJoinBtn ? (
