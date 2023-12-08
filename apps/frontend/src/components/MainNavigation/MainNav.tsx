4d123be40926320f26d74fc87d1190ad1521c915
Radu Constantin <raduadrian.constantin94@gmail.com>
2023-12-08 13:31:12 +0200

    Added functional log out button

--- apps/frontend/src/components/MainNavigation/MainNav.tsx
+++ apps/frontend/src/components/MainNavigation/MainNav.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Nav, Navbar } from 'react-bootstrap';
 import styles from './MainNav.module.css';
-import { getStoredUser } from '../../utils/auth';
+import { getStoredUser, deleteStoredUser } from '../../utils/auth';
 import UserCircle from '../UserCircle/UserCircle';
+
 
 // const profilePic = require('./profile_placeholder.png');
 
@@ -52,6 +55,20 @@ const MainNav = () => {
         <path d="M256 32V51.2C329 66.03 384 130.6 384 208V226.8C384 273.9 401.3 319.2 432.5 354.4L439.9 362.7C448.3 372.2 450.4 385.6 445.2 397.1C440 408.6 428.6 416 416 416H32C19.4 416 7.971 408.6 2.809 397.1C-2.353 385.6-.2883 372.2 8.084 362.7L15.5 354.4C46.74 319.2 64 273.9 64 226.8V208C64 130.6 118.1 66.03 192 51.2V32C192 14.33 206.3 0 224 0C241.7 0 256 14.33 256 32H256zM224 512C207 512 190.7 505.3 178.7 493.3C166.7 481.3 160 464.1 160 448H288C288 464.1 281.3 481.3 269.3 493.3C257.3 505.3 240.1 512 224 512z" />
       </svg>
     </div>
+  )
+
+  const logoutIconLink = (
+    <div className={styles.link}>
+      <FontAwesomeIcon
+              icon={faRightFromBracket}
+              size="2xl"
+              className={styles.leaveIcon}
+              onClick={() => {
+                deleteStoredUser()
+                window.location.reload()
+              }}
+            />
+    </div>
   );
 
   return (
@@ -75,6 +92,12 @@ const MainNav = () => {
               <Nav.Link href="#pricing" className={styles.navbarCollapseLink}>
                 NOTIFICATIONS
               </Nav.Link>
+              <Nav.Link className={styles.navbarCollapseLink} onClick={() => {
+                  deleteStoredUser()
+                  window.location.reload()
+              }}>
+                LOG OUT
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </>
@@ -84,6 +107,7 @@ const MainNav = () => {
           {homeIconLink}
           {exploreIconLink}
           {notificationsIconLink}
+          {logoutIconLink}
         </>
       )}
     </Navbar>
