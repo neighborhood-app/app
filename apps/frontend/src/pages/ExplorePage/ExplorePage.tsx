2b8b8fdb720800e5ac530afd7aa3edd6841eec28
Radu Constantin <raduadrian.constantin94@gmail.com>
2023-12-29 19:10:50 +0200

    Added toggle to explore page

--- apps/frontend/src/pages/ExplorePage/ExplorePage.tsx
+++ apps/frontend/src/pages/ExplorePage/ExplorePage.tsx
@@ -1,5 +1,8 @@
 import { useLoaderData, LoaderFunctionArgs } from 'react-router';
+import Switch from '@mui/material/Switch';
 import { NeighborhoodsPerPage } from '@neighborhood/backend/src/types';
+import { useState } from 'react';
+import styles from './ExplorePage.module.css';
 import neighborhoodsService from '../../services/neighborhoods';
 import NeighborhoodSearch from '../../components/NeighborhoodSearch/NeighborhoodSearch';
 
@@ -11,10 +14,29 @@ export async function loader({ params }: LoaderFunctionArgs) {
 export default function ExplorePage() {
   const { neighborhoods, newCursor, hasNextPage } = useLoaderData() as NeighborhoodsPerPage;
 
+  /*
+  If checked, page will display the map view, if unchecked it will display the list view;
+  Default is list view.
+  */
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+
   return (
-    <NeighborhoodSearch
-      neighborhoods={neighborhoods}
-      cursor={newCursor}
-      isNextPage={hasNextPage}></NeighborhoodSearch>
+    <div className={styles.wrapper}>
+      <div className={styles.switchContainer}>
+        <p>List</p>
+        <Switch className={styles.switch} size="medium" checked={checked} onChange={handleChange} />
+        <p>Map</p>
+      </div>
+      {checked ? null : (
+        <NeighborhoodSearch
+          neighborhoods={neighborhoods}
+          cursor={newCursor}
+          isNextPage={hasNextPage}></NeighborhoodSearch>
+      )}
+    </div>
   );
 }
