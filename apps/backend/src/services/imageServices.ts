b2afa35915d5573685b8b4854a65fd25a8cf5ea3
nina-dk <antwnina.k@gmail.com>
2024-03-22 18:00:17 +0200

    Comment out unused functions

--- apps/backend/src/services/imageServices.ts
+++ apps/backend/src/services/imageServices.ts
@@ -30,41 +30,41 @@ const uploadImage = async (imagePath: File | string, publicId: string): Promise<
 };
 
 // Gets details of an uploaded image //
-const getAssetInfo = async (publicId: number) => {
-  // Return colors in the response
-  const options = {
-    colors: true,
-  };
+// const getAssetInfo = async (publicId: number) => {
+//   // Return colors in the response
+//   const options = {
+//     colors: true,
+//   };
 
-  try {
-    // Get details about the asset
-    const result = await cloudinary.api.resource(publicId, options);
-    console.log(result);
-    return result.colors;
-  } catch (error) {
-    console.error(error);
-  }
-};
+//   try {
+//     // Get details about the asset
+//     const result = await cloudinary.api.resource(publicId, options);
+//     console.log(result);
+//     return result.colors;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
 
 // Creates an HTML image tag with a transformation that
 // results in a circular thumbnail crop of the image
 // focused on the faces, applying an outline of the
 // first color, and setting a background of the second color.
-const createImageTag = (publicId: number, ...colors: string[]) => {
-  // Set the effect color and background color
-  const [effectColor, backgroundColor] = colors;
+// const createImageTag = (publicId: number, ...colors: string[]) => {
+//   // Set the effect color and background color
+//   const [effectColor, backgroundColor] = colors;
 
-  // Create an image tag with transformations applied to the src URL
-  const imageTag = cloudinary.image(publicId, {
-    transformation: [
-      { width: 250, height: 250, gravity: 'faces', crop: 'thumb' },
-      { radius: 'max' },
-      { effect: 'outline:10', color: effectColor },
-      { background: backgroundColor },
-    ],
-  });
+//   // Create an image tag with transformations applied to the src URL
+//   const imageTag = cloudinary.image(publicId, {
+//     transformation: [
+//       { width: 250, height: 250, gravity: 'faces', crop: 'thumb' },
+//       { radius: 'max' },
+//       { effect: 'outline:10', color: effectColor },
+//       { background: backgroundColor },
+//     ],
+//   });
 
-  return imageTag;
-};
+//   return imageTag;
+// };
 
-export { uploadImage, getAssetInfo, createImageTag };
+export { uploadImage };
