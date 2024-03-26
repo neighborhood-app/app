a06bf53282f630fb6a0651c2fa3619840d6ac1ce
Radu Constantin <raduadrian.constantin94@gmail.com>
2024-03-26 16:23:21 +0200

    Added conditional statement for dotenv package

--- apps/backend/src/utils/config.ts
+++ apps/backend/src/utils/config.ts
@@ -1,6 +1,7 @@
-import { config } from 'dotenv';
-
-config();
+if (process.env.NODE_ENV !== 'production') {
+	// eslint-disable-next-line global-require
+	require('dotenv').config();
+}
 
 const { DATABASE_URL, PORT, SECRET } = process.env;
 
