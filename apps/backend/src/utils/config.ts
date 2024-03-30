73959b93390b82a5e6dffc4f031cc31e0c9fd400
Radu Constantin <raduadrian.constantin94@gmail.com>
2024-03-30 11:38:15 +0200

    Removed if statement from config.ts

--- apps/backend/src/utils/config.ts
+++ apps/backend/src/utils/config.ts
@@ -1,7 +1,11 @@
-if (process.env.NODE_ENV !== 'production') {
-	// eslint-disable-next-line global-require
-	require('dotenv').config();
-}
+// if (process.env.NODE_ENV !== 'production') {
+// 	// eslint-disable-next-line global-require
+// 	require('dotenv').config();
+// }
+
+import { config } from 'dotenv';
+
+config();
 
 const { DATABASE_URL, PORT, SECRET } = process.env;
 
